8f02476f1dc6cbf5469dc10b26575d9b3dd559a0
anime-song <op_2016_11_16@yahoo.co.jp>
2019-09-15 21:42:54 +0900

    commit

--- docs/js/sendrequest.js
+++ docs/js/sendrequest.js
@@ -7,7 +7,7 @@ function send() {
     console.log(request_data)
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://chord-key-detection.herokuapp.com/');
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send(request_data);
 
     xhr.onreadystatechange = function () {
@@ -17,6 +17,8 @@ function send() {
         if (this.readyState == READYSTATE_COMLETED && this.status == HTTP_STATUS_OK) {
             alert(this.responseText);
             $('[name=result]').val(this.responseText);
+        } else{
+            alert("通信失敗");
         }
     };
 };
