dd775358a7bc656f8c78abc22e45e3ce8b5133a2
anime-song <op_2016_11_16@yahoo.co.jp>
2019-09-15 21:28:31 +0900

    commit

---
+++ docs/js/sendrequest.js
@@ -0,0 +1,22 @@
+function send() {
+    var form = {};
+    form.chords = ["Cm7", "Bm7"]
+    console.log(form)
+
+    var request_data = JSON.stringify(form);
+    console.log(request_data)
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://chord-key-detection.herokuapp.com/');
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+    xhr.send(request_data);
+
+    xhr.onreadystatechange = function () {
+        var READYSTATE_COMLETED = 4;
+        var HTTP_STATUS_OK = 200;
+
+        if (this.readyState == READYSTATE_COMLETED && this.status == HTTP_STATUS_OK) {
+            alert(this.responseText);
+            $('[name=result]').val(this.responseText);
+        }
+    };
+};
