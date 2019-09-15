3aed19f08473c455691af2b1e09f0aa8822b6ff1
anime-song <op_2016_11_16@yahoo.co.jp>
2019-09-16 01:10:37 +0900

    commit

--- docs/js/sendrequest.js
+++ docs/js/sendrequest.js
@@ -1,3 +1,7 @@
+const API_URL = 'https://chord-key-detection.herokuapp.com/'
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_BAD_REQUEST = 400;
+
 function sort(x){
     var keys=[];
     for(var key in x)keys.push(key);
@@ -24,7 +28,7 @@ function parse(x){
         sort_data.push({"key": sort_keys[i], "score": (data[sort_keys[i]] * 100.0).toFixed(1)})
     }
 
-    console.log(sort_data);
+    console.log(sort_data)
 
     return data
 };
@@ -32,8 +36,6 @@ function parse(x){
 function send() {
     var http_status;
     var result_data;
-    var HTTP_STATUS_OK = 200;
-    var HTTP_STATUS_BAD_REQUEST = 400;
 
     var form = {};
     form.chords = document.getElementById("chords").value.split(" ")
@@ -43,7 +45,7 @@ function send() {
     var request_data = JSON.stringify(form);
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://chord-key-detection.herokuapp.com/');
+    xhr.open('POST', API_URL);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send(request_data);
 
